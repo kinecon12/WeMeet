6f119981849bb7f1f15e9dcf89ab5024230de513
ikejemba <kinecon12@gmail.com>
2025-01-21 23:17:18 +0000

    updating our home function to be able filter our roomsby user, host or room.id through search form

--- WeMeet/roots/views.py
+++ WeMeet/roots/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 def home(request):
-  rooms = Room.objects.all()
-  contest = {'rooms': rooms}
+  q = request.GET.get('q') if request.GET.get('q') != None else ''
+  rooms = Room.objects.all(topic__name__icontains=q)
+  topics = Topic.objects.all()
+  contest = {'rooms': rooms, 'topics': topics}
   return render(request,'roots/home.html', contest)
   
 def room(request, pk):
