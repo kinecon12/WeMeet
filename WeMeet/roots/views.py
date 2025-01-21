5ac83208a9e26a1c3233b2cf3a83bab842953c17
ikejemba <kinecon12@gmail.com>
2025-01-21 13:15:01 +0000

    dynamic url routing of home and room templates

--- WeMeet/roots/views.py
+++ WeMeet/roots/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+  {'id': 1, 'name': 'Room 1'},
+  {'id': 2, 'name': 'Room 2'},
+  {'id': 3, 'name': 'Room 3'},
+]
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    contest = {'rooms': rooms}
+    return render(request,'roots/home.html', contest)
   
 def room(request):
-    return HttpResponse('This is a room!')
+    return render(request, 'roots/room.html')
 # Create your views here.
