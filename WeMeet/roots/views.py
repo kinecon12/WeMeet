5a874065bf4a81d315c07ae6b115778d81b45967
ikejemba <kinecon12@gmail.com>
2025-01-21 11:57:01 +0000

    creation of template folder, room.html and home.html

--- WeMeet/roots/views.py
+++ WeMeet/roots/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def home(request):
+    return HttpResponse('Hello, World!')
+  
+def room(request):
+    return HttpResponse('This is a room!')
 # Create your views here.
