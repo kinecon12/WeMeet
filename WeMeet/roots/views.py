2809859f0a1178b5d5e99321ff611061802c6917
ikejemba <kinecon12@gmail.com>
2025-01-22 10:36:02 +0000

    import a class methode decorator 'login_required' to handle some access control to some defined function

--- WeMeet/roots/views.py
+++ WeMeet/roots/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required   
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
@@ -13,8 +16,20 @@ def loginPage(request):
       user = User.objects.get(username=username)
     except:
       messages.error(request, 'User does not exit')
+    
+    user = authenticate(request, username=username, password=password)
+    
+    if user is not None:
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, 'Username OR password is incorrect')
   contest = {}
   return render(request, 'roots/login_register.html', contest)
+
+def logoutUser(request):
+  logout(request)
+  return redirect('home')
 
 def home(request):
   q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -28,7 +43,8 @@ def room(request, pk):
   rooms = Room.objects.get(id=pk)
   contest = {'room': room}
   return render(request, 'roots/room.html', contest)
-# Create your views here.
+
+@login_required(login_url= 'login')
 def createRoom(request):
   form = RoomForm()
   if request.method == 'POST':
