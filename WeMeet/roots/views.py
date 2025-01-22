a751936b7af075b5cb81e2d3cdcdb945a4654008
ikejemba <kinecon12@gmail.com>
2025-01-22 12:23:18 +0000

    updating the room function and template to have functionality for user to comment on any room

--- WeMeet/roots/views.py
+++ WeMeet/roots/views.py
@@ -6,12 +6,17 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
 def loginPage(request):
+  page = 'login'
+  if request.user.is_authenticated:
+    return redirect('home')
+  
   if request.method == 'POST':
-    username = request.POST.get('username')
+    username = request.POST.get('username').lower()
     password = request.POST.get('password')
     try:
       user = User.objects.get(username=username)
@@ -25,12 +30,27 @@ def loginPage(request):
       return redirect('home')
     else:
       messages.error(request, 'Username OR password is incorrect')
-  contest = {}
+  contest = {'page': page }
   return render(request, 'roots/login_register.html', contest)
 
 def logoutUser(request):
   logout(request)
   return redirect('home')
+
+def registerPage(request):
+  form = UserCreationForm()
+  
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.username = user.username.lower()
+      user.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, 'An error has occured during registration')
+  return render(request, 'roots/login_register.html', {'form': form})
 
 def home(request):
   q = request.GET.get('q') if request.GET.get('q') != None else ''
