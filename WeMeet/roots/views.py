bafbc94d1b99342faaa80692c2627017313b62a2
ikejemba <kinecon12@gmail.com>
2025-01-23 13:58:17 +0000

    loading style sheet to home.html

--- WeMeet/roots/views.py
+++ WeMeet/roots/views.py
@@ -92,7 +92,9 @@ def createRoom(request):
   if request.method == 'POST':
     form = RoomForm(request.POST)
     if form.is_valid():
-      form.save()
+      room = form.save(commit=False)
+      room.host = request.user
+      room.save()
       return redirect('home')
   contest = {'form': form}
   return render(request, 'roots/room_form.html', contest)
